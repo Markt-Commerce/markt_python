1b011cb4566626f26523d2ae97aeb5b483db5fa9
Adebowale Morakinyo <adebowalemorakinyo@gmail.com>
2025-05-28 20:06:41 +0100

    fix(task): removed test values

--- main/schedules.py
+++ main/schedules.py
@@ -6,12 +6,12 @@ def configure_schedules(app):
     celery_app.conf.beat_schedule = {
         "generate-feeds": {
             "task": "app.socials.tasks.generate_all_feeds",
-            "schedule": crontab(hour=0, minute=1),
+            "schedule": crontab(hour=1, minute=0),
             "options": {"queue": "social"},
         },
         "update-trending": {
             "task": "app.socials.tasks.update_popular_content",
-            "schedule": crontab(minute="*/03"),
+            "schedule": crontab(minute="*/30"),
             "options": {"queue": "social"},
         },
     }
